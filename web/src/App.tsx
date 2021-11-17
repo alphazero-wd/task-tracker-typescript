54a4b3f95605fe610c8913fbf60d28881e40c853
Alphazero <alphazero25811@gmail.com>
2021-11-17 13:27:21 +0700

    User Account

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,14 @@
-import { ChakraProvider, CSSReset } from "@chakra-ui/react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import ForgotPassword from "./pages/ForgotPassword";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import ResetPassword from "./pages/ResetPassword";
-import SignUp from "./pages/SignUp";
-import { useAppSelector } from "./store";
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ForgotPassword from './pages/ForgotPassword';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import ResetPassword from './pages/ResetPassword';
+import SignUp from './pages/SignUp';
+import { useAppSelector } from './store';
 function App() {
-  const { user } = useAppSelector(state => state.user);
+  const { user } = useAppSelector((state) => state.user);
   return (
     <ChakraProvider>
       <CSSReset />
@@ -16,24 +16,24 @@ function App() {
         <Navbar />
         <Routes>
           <Route
-            path="/"
+            path="/tasks"
             element={user ? <Home /> : <Navigate to="/signup" />}
           />
           <Route
             path="/signup"
-            element={!user ? <SignUp /> : <Navigate to="/" />}
+            element={!user ? <SignUp /> : <Navigate to="/tasks" />}
           />
           <Route
             path="/login"
-            element={!user ? <Login /> : <Navigate to="/" />}
+            element={!user ? <Login /> : <Navigate to="/tasks" />}
           />
           <Route
             path="/forgot-password"
-            element={user ? <Navigate to="/" /> : <ForgotPassword />}
+            element={user ? <Navigate to="/tasks" /> : <ForgotPassword />}
           />
           <Route
             path="/reset-password/:token"
-            element={user ? <Navigate to="/" /> : <ResetPassword />}
+            element={user ? <Navigate to="/tasks" /> : <ResetPassword />}
           />
         </Routes>
       </BrowserRouter>
