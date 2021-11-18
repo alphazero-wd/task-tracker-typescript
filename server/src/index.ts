24b3e481d94adda993b72213eb4738208efc3e87
Alphazero <alphazero25811@gmail.com>
2021-11-18 16:33:36 +0700

    Migrations and title

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,23 @@
-import "reflect-metadata";
-import "dotenv/config";
-import { createConnection } from "typeorm";
-import express from "express";
-import { Task } from "./models/Task";
-import tasksRouter from "./routes/tasks";
-import userRouter from "./routes/user";
-import { User } from "./models/User";
-import { errorHandler } from "./middlewares/errorHandler";
-import cors from "cors";
-import { __prod__ } from "./utils/constants";
+import 'reflect-metadata';
+import 'dotenv/config';
+import { createConnection } from 'typeorm';
+import express from 'express';
+import { Task } from './models/Task';
+import tasksRouter from './routes/tasks';
+import userRouter from './routes/user';
+import { User } from './models/User';
+import { errorHandler } from './middlewares/errorHandler';
+import cors from 'cors';
+import { __prod__ } from './utils/constants';
 (async () => {
-  await createConnection({
-    type: "postgres",
+  const connection = await createConnection({
+    type: 'postgres',
     entities: [Task, User],
     url: process.env.CONNECTION_URI,
     logging: !__prod__,
     synchronize: !__prod__,
   });
-
+  await connection.runMigrations();
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
@@ -26,17 +26,17 @@ import { __prod__ } from "./utils/constants";
   app.use(
     cors({
       origin: __prod__
-        ? "https://mern-task-tracker.netlify.app"
-        : "http://localhost:3000",
+        ? 'https://mern-task-tracker.netlify.app'
+        : 'http://localhost:3000',
     })
   );
 
-  app.get("/", (_, res) => {
-    res.send("Hello World");
+  app.get('/', (_, res) => {
+    res.send('Hello World');
   });
 
-  app.use("/api/tasks", tasksRouter);
-  app.use("/api/user", userRouter);
+  app.use('/api/tasks', tasksRouter);
+  app.use('/api/user', userRouter);
   app.use(errorHandler);
 
   const PORT = process.env.PORT || 5000;
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -6,21 +6,21 @@ import {
   OneToMany,
   Entity,
   BaseEntity,
-} from "typeorm";
-import { Task } from "./Task";
+} from 'typeorm';
+import { Task } from './Task';
 
 @Entity()
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   userId!: number;
 
-  @Column({ type: "varchar", length: 64, unique: true })
+  @Column({ type: 'varchar', length: 64, unique: true })
   username!: string;
 
-  @Column({ type: "varchar", length: 255, unique: true })
+  @Column({ type: 'varchar', length: 255, unique: true })
   email!: string;
 
-  @Column({ type: "varchar", length: 255 })
+  @Column({ type: 'varchar', length: 255 })
   password!: string;
 
   @CreateDateColumn()
@@ -29,9 +29,6 @@ export class User extends BaseEntity {
   @UpdateDateColumn()
   updatedAt: Date;
 
-  @OneToMany(() => Task, task => task.userId)
+  @OneToMany(() => Task, (task) => task.userId)
   tasks: Task[];
-
-  @Column({default: false})
-  isAdmin: boolean;
 }
